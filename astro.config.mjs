7c7de96df0f62826868fdcde53eadd91c77bcc49
mizuka123 <mizuka123@users.noreply.github.com>
2023-11-14 03:22:59 +0000

    Format by GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
@@ -16,16 +16,27 @@ const getSite = function () {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
-    return new URL(BASE_PATH, `https://${new URL(process.env.CF_PAGES_URL).host.split('.').slice(1).join('.')}`).toString();
+    return new URL(
+      BASE_PATH,
+      `https://${new URL(process.env.CF_PAGES_URL).host
+        .split('.')
+        .slice(1)
+        .join('.')}`
+    ).toString();
   }
 
   return new URL(BASE_PATH, 'http://localhost:4321').toString();
 };
 
-
 // https://astro.build/config
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
-  integrations: [CoverImageDownloader(), CustomIconDownloader(), FeaturedImageDownloader(), PublicNotionCopier(), sitemap()]
+  integrations: [
+    CoverImageDownloader(),
+    CustomIconDownloader(),
+    FeaturedImageDownloader(),
+    PublicNotionCopier(),
+    sitemap(),
+  ],
 });
